90be86788fd23ca87c2744837a3dfe2b3d3a4fb5
farheenhasanhasan56-cpu <farheenhasanhasan56@gmail.com>
2026-07-14 15:29:09 +0530

    new page add

---
+++ Controllers/MasterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regis.Controllers
+{
+    public class MasterController : Controller
+    {
+        // GET: Master
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CategoryMaster()
+        {
+            return View();
+        }
+    }
+}
