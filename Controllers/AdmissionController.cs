5c1b92e5f79762ec4c5cea3c59c50690051bafed
farheenhasanhasan56-cpu <farheenhasanhasan56@gmail.com>
2026-07-15 16:12:32 +0530

    Created Admission and Dashboard controllers along with their respective pages

---
+++ Controllers/AdmissionController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regis.Controllers
+{
+    public class AdmissionController : Controller
+    {
+        // GET: Admission
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AdmissionSetup()
+        {
+            return View();
+        }
+
+        public ActionResult EligibilityCheck()
+        {
+            return View();
+        }
+
+        public ActionResult StudentRegistration()
+        {
+            return View();
+        }
+
+        public ActionResult Application()
+        {
+            return View();
+        }
+
+        public ActionResult DocumentVerification()
+        {
+            return View();
+        }
+
+        public ActionResult Counselling()
+        {
+            return View();
+        }
+
+        public ActionResult FeePayment()
+        {
+            return View();
+        }
+
+        public ActionResult Final()
+        {
+            return View();
+        }
+    }
+}
