df72ff0157bae371cae7db8284d29e8eacbdab98
farheenhasanhasan56-cpu <farheenhasanhasan56@gmail.com>
2026-07-15 11:16:42 +0530

    added new new controller and views

---
+++ Controllers/SeatManagementController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regis.Controllers
+{
+    public class SeatManagementController : Controller
+    {
+        // GET: SeatManagementt
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SeatMatrix()
+        {
+            return View();
+        }
+        public ActionResult CategorywiseSeats()
+        {
+            return View();
+        }
+        public ActionResult SeatAllotment()
+        {
+            return View();
+        }
+    }
+}
