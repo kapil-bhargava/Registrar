9218f1feb7c1906967eca92e5fe90263ff41e086
farheenhasanhasan56-cpu <farheenhasanhasan56@gmail.com>
2026-07-16 11:46:03 +0530

    reports and student master pages +controllers

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regis.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AdmissionReports()
+        {
+            return View();
+        }
+
+        // Seat Allotment Report
+        public ActionResult SeatAllotmentReport()
+        {
+            return View();
+        }
+
+        // Student Master Report
+        public ActionResult StudentMasterReport()
+        {
+            return View();
+        }
+
+        // Academic Reports
+        public ActionResult AcademicReports()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StudentMasterController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regis.Controllers
+{
+    public class StudentMasterController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult StudentRecords()
+        {
+            return View();
+        }
+
+        // Student Mapping
+        public ActionResult StudentMapping()
+        {
+            return View();
+        }
+
+        // Identity Generation
+        public ActionResult IdentityGeneration()
+        {
+            return View();
+        }
+
+        // Academic Progress
+        public ActionResult AcademicProgress()
+        {
+            return View();
+        }
+
+        // Certificate Management
+        public ActionResult CertificateManagement()
+        {
+            return View();
+        }
+
+        // Alumni
+        public ActionResult Alumni()
+        {
+            return View();
+        }
+    }
+}
